f88783f55f9db0558bd7f7fd65934fd1ac440ce8
Anthony Sutton <anthony.sutton95@gmail.com>
2020-03-12 16:05:37 -0700

    Player can pick up items found within the room

--- src/adv.py
+++ src/adv.py
@@ -15,17 +15,16 @@ passages run north and east."""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""", Item("axe", """Used to
-    chop wood. Mostly...""")),
+the distance, but there is no way across the chasm.""",
+                     Item("axe", """Used to chop wood. Mostly...""")),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""", [Item("wand", """What?
-    Do you think you're a wizard?"""), Item("coin", """You find a coin
-    that has been left behind.""")]),
+earlier adventurers. The only exit is to the south.""", Item("coin", """You find
+    a coin that has been left behind.""")),
 }
 
 
@@ -66,7 +65,8 @@ print(player)
 print(player.current_room.description + "\n")
 print("Command Table\n" + "=" * 30)
 print("Travel: (n) North (e) East (s) South (w) West")
-print("Player: (i) Inventory (l) Locate items (q) quit")
+print("""Player: (i) Inventory (l) Locate items (get) Items
+        (drop) Items (q) quit""")
 print("\nWhat would you like to do?")
 
 while True:
@@ -76,6 +76,7 @@ while True:
     # Sets the available directions for the user
     acceptable_travel_directions = ['n', 'e', 's', 'w']
     acceptable_player_actions = ['i', 'inventory']
+    current_room = player.current_room
 
     print()
     print("=" * 20 + "\n")
@@ -86,6 +87,19 @@ while True:
         player.items_in_inventory()
     elif player_input.lower() == "l":
         player.current_room.items_in_room()
+    elif "get" in player_input.lower():
+        print("Working...")
+        player_words = player_input.split()
+
+        if len(player_words) > 1:
+            player_item_selection = player_words[1]
+            found_item = current_room.does_item_exist(player_item_selection)
+
+            print(found_item)
+            if found_item:
+                player.add_item(found_item)
+                current_room.remove_item(found_item)
+
     elif player_input == 'q':
         os.system("clear")
         print("Thank you for playing!\n")
--- src/room.py
+++ src/room.py
@@ -5,35 +5,45 @@ from player import Player
 
 
 class Room:
-    def __init__(self, name, description, items=[]):
+    def __init__(self, name, description, *items):
         self.name = name
         self.description = description
-        self.items = []
-        if len(self.items) > 0:
-            for item in self.items:
-                self.items.append(item)
-        else:
-            self.items = items
+        self.item_list = []
+
+        if len(items) > 0:
+            for item in items:
+                self.item_list.append(item)
 
     def __str__(self):
         return (f"{self.name}")
 
+    def remove_item(self, item):
+        self.item_list.remove(item)
+
     def items_in_room(self):
         # Checks if an item list of a room is empty and returns an error
-        if self.items == []:
-            print("No item(s) were found in this room.")
-
-        # Checks if their is a list of items, and prints them out one, by one
-        elif type(self.items) is list:
-            for item in self.items:
+        if self.item_list == []:
+            print("You find no items in the room.")
+        # Return the items that are found
+        else:
+            for item in self.item_list:
                 print(f"You find a(n) {item.name}.")
 
-        # Else, just prints the single item found
-        else:
-            print(f"You find a(n) {self.items.name}.")
+    def does_item_exist(self, item_name):
+        for item in self.item_list:
+            if item_name.lower() == item.name.lower():
+                return item
+            else:
+                print("That item does not exist.")
 
     def pickup_item(self, item):
-        self.items.remove(item)
-        Player.add_item(item)
-
+        if type(self.items) is list:
+            for item in self.items:
+                self.items.remove(item)
+                Player.add_item(item)
+        elif self.items == []:
+            print("That item does not exist")
+        else:
+            print(self.items)
+            Player.add_item(self.items)
     # TODO: Add list of exists that are available.
